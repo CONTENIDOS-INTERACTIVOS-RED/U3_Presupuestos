138bf3f582504334afd57e9df9c85da9540aca2c
Daniel Francisco Alvarez Caballero <95438055+DanielFrnAlvarez@users.noreply.github.com>
2025-03-09 18:25:56 -0500

    Initial commit

---
+++ src/views/curso/Tema1.vue
@@ -0,0 +1,180 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(:subTitulo="'NOMBRE TEMA 3'")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
+    #t_1_1.titulo-segundo
+      div.d-flex
+        span.me-3 1.1  
+        h2 Título de segundo
+    .row.justify-content-between.mb-5
+      .col-md-6.col-lg-7
+        p Desde los inicios de la humanidad, las reglas han sido un elemento fundamental para el desarrollo de las sociedades. El Derecho, en este contexto, se presenta como una herramienta indispensable para el buen funcionamiento de la convivencia. En esta primera unidad, se profundizará en el concepto de Derecho, su propósito y origen, así como en el proceso de creación de normas que regulan nuestras actividades diarias, las relaciones entre individuos y el Estado, y las normas que operan en el ámbito global.
+      .col-md-6.col-lg-5
+        //- .imagen-titulo--der debe ir acompañado de una de una de estas clases => 
+        //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+        figure.mt-4.mt-lg-0
+          img(src='@/assets/componentes/banner-img-1.png', alt='Texto que describa la imagen')
+    #t_1_2.titulo-segundo.color-secundario
+      h2 1.2  Acordeón
+    .row.justify-content-between.mb-5
+      .col-md-6.col-lg-7
+        AcordionA(tipo="b")
+          .row(titulo="Titulo item 1")
+            .col
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+
+          div(titulo="Titulo item 2")
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+              figcaption Leyenda de la imagen
+      .col-md-6.col-lg-5.col-xl-4.mx-xl-auto
+        //- .imagen-titulo--der debe ir acompañado de una de una de estas clases => 
+        //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+        figure
+          img(src='@/assets/componentes/banner-img-2.png', alt='Texto que describa la imagen')
+    #t_1_3.titulo-segundo.color-secundario.pb-5
+      h2 1.3  Slider
+    .row.position-relative.mb-5
+      img.bg-img-1(src='../../assets/componentes/background-test.svg', alt='Texto que describa la imagen')
+      SlyderA(tipo="b").bg-white
+        .row.p-4.p-md-5
+          .col-md-6.col-lg-7.mb-4.mb-md-0
+            p.text-bold Coercibilidad
+            p El derecho puede imponerse por la fuerza si es necesario. Cuando una persona incumple una norma jurídica, puede ser sancionada mediante multas, penas de prisión o la confiscación de bienes. Esta coercibilidad distingue al derecho de otros sistemas normativos que no cuentan con mecanismos de imposición obligatoria.
+        
+          .col-md-6.col-lg-5
+            figure
+              img(src='@/assets/componentes/banner-img-3.png', alt='Texto que describa la imagen')
+        .row.p-4.p-md-5
+          .col-md-6.mb-4.mb-md-0
+            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+        
+          .col-md-6
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+              figcaption Leyenda de la imagen
+        .row.p-4.p-md-5
+          .col-md-6.mb-4.mb-md-0
+            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+          .col-md-6
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+              figcaption Leyenda de la imagen
+    #t_1_4.titulo-segundo.color-secundario.pt-5
+      h2 1.4  Pestañas
+    .row.mb-5
+      TabsC.color-primario
+        .tabs-content.border.px-4.p-md-5.py-3(titulo="The splendor of the mystery")
+          .row
+            .col-md-6.col-lg-7.col-xl-8.mb-4.mb-md-0
+              h4 Celebrate your failures 1
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+          
+            .col-md-6.col-lg-5.col-xl-4
+              figure
+                img(src='@/assets/componentes/banner-img-4.png', alt='Texto que describa la imagen')
+        .tabs-content.border.px-4.p-md-5.py-3(titulo="Jerarquía normativa")
+          .row
+            .col-md-6.col-lg-7.col-xl-8.mb-4.mb-md-0
+              h4 Celebrate your failures 1
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+          
+            .col-md-6.col-lg-5.col-xl-4
+              figure
+                img(src='@/assets/componentes/banner-img-4.png', alt='Texto que describa la imagen')
+        .tabs-content.border.px-4.p-md-5.py-3(titulo="Sentido amplio de la norma")
+          .row
+            .col-md-6.col-lg-7.col-xl-8.mb-4.mb-md-0
+              h4 Celebrate your failures 1
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+          
+            .col-md-6.col-lg-5.col-xl-4
+              figure
+                img(src='@/assets/componentes/banner-img-4.png', alt='Texto que describa la imagen')
+    #t_1_5.titulo-segundo.color-secundario.pt-5
+      h2 1.5  Infografia interactiva
+      
+    .row.mb-5.justify-content-center.img-infografica.p-md-5
+      .col-12.col-md-9
+        ImagenInfografica.color-secundario
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/componentes/test-img-infogr.svg', alt='Texto que describa la imagen')
+
+          .tarjeta.color-acento-botones.p-3(x="9%" y="19%" numero="+")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+
+          .tarjeta.color-acento-botones.p-3(x="91%" y="81%" numero="+")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor ddasdasd asdasd asdasd a asd sd asdasd sit amet, consectetur adipiscing elit. Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+
+          .tarjeta.color-acento-botones.p-3(x="91%" y="19%" numero="+")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+
+          .tarjeta.color-acento-botones.p-3(x="9%" y="81%" numero="+")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+    
+    #t_1_6.titulo-segundo
+      div.d-flex
+        span.me-3 1.6  
+        h2 Linea de tiempo D
+
+    LineaTiempoD.color-secundario.mb-5
+      p(numero="1" titulo="Praesent luctus") Lorem ipsum dolor sit amet, 
+      p(numero="2" titulo="Praesent luctus") Lorem ipsum dolor sit amet, 
+      p(numero="3" titulo="Praesent luctus") Lorem ipsum dolor sit amet, 
+    
+    #t_1_7.titulo-segundo
+      div.d-flex
+        span.me-3 1.7
+        h2 Tarjeta de audio
+    
+      TarjetaAudio.color-primario.bg-white.mb-5(
+        texto="Proceso de descarga de MySQL Server Community y MySQL Workbench"
+        tiempo
+        :audio="require('../../assets/componentes/audios/audio-ej.mp3')"
+        @audio-hover="mostrarIndicadorTarjetaAudio = false"
+      )
+
+    .bg-full-width.border-top-9-azul
+      .px-4.p-md-5
+        h2 Material complementario
+        .row
+          .col-12.col-md-6.col-lg-8
+            p.text-small
+            p.d-flex.my-4
+              img.me-3.mb-auto.mt-2(src='@/assets/componentes/youtube-icon.svg' :style="{'max-width':'16px'}")
+              span.text-small
+            p.d-flex.my-4
+              img.me-3.mb-auto.mt-2(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              span.text-small
+            p.d-flex.my-4
+              img.me-3.mb-auto.mt-2(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              span.text-small
+          .col-12.col-md-6.col-lg-3
+            figure
+              img(src='@/assets/componentes/material-complementario.svg', alt='Texto que describa la imagen')
+</template>
+
+<script>
+export default {
+  name: 'Tema1',
+  data: () => ({}),
+
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+}
+</script>
+
+<style lang="sass">
+.tabs-content
+  background-color: $color-sistema-h
+.img-infografica
+  background-color: $color-adicional-1
+</style>
---
+++ src/views/curso/Tema3.vue
@@ -0,0 +1,283 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(:subTitulo="'NOMBRE TEMA 3'")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    #t_3_1.titulo-segundo
+      h2 3.1 Carrusel
+    .row.justify-content-center.mb-5
+      .col-11.col-md-4.col-xl-5.pe-xl-8
+        figure.mb-4.mb-md-0
+          img(src='@/assets/componentes/test-img-carousel.png', alt='Texto que describa la imagen')
+      .col-11.col-md-8.col-xl-7
+        SlyderF(columnas="col-12 col-lg-6")
+          .row.py-4.px-3.h-100
+            .col-10.offset-1.offset-md-0.col-md-12.shadow-app.p-3.py-md-5.px-md-4
+              .row.justify-content-center
+                .col-5.col-md-4.mb-4
+                  img(src='@/assets/componentes/test-slider.svg' alt='AvatarTop')
+                h3.text-center.mb-4 Ley 8 de 1922
+                p.text-center Introdujo la separación de bienes.
+          .row.py-4.px-3.h-100
+            .col-10.offset-1.offset-md-0.col-md-12.shadow-app.p-3.py-md-5.px-md-4
+              .row.justify-content-center
+                .col-5.col-md-4.mb-4
+                  img(src='@/assets/componentes/test-slider.svg' alt='AvatarTop')
+                h3.text-center.mb-4 Ley 54 de 1924
+                p.text-center Permitió el matrimonio civil entre católicos que declaran  haberse separado de la Iglesia.
+          .row.py-4.px-3.h-100
+            .col-10.offset-1.offset-md-0.col-md-12.shadow-app.p-3.py-md-5.px-md-4
+              .row.justify-content-center
+                .col-5.col-md-4.mb-4
+                  img(src='@/assets/componentes/test-slider.svg' alt='AvatarTop')
+                h3.text-center.mb-4 Ley 8 de 1922
+                p.text-center Introdujo la separación de bienes.
+
+    #t_3_2.titulo-segundo
+      h2 3.2 Tablas
+
+    .row
+      .col-xl-9
+        .tabla-a.mb-5
+          table
+            caption Leyenda de la tabla
+            thead
+              tr
+                th Ventajas
+                th Desafios
+            tbody
+              tr
+                td <strong>Flexibilidad de tiempo y lugar:</strong>Los estudiantes pueden acceder a los cursos desde cualquier lugar y en el momento que mejor se ajuste a su agenda.
+                td <strong>Autogestión   del tiempo:</strong>Requiere que los estudiantes sean disciplinados y tengan la capacidad de organizar su tiempo de manera efectiva.
+              tr
+                td <strong>Acceso global: </strong>Cualquier persona con conexión a internet puede acceder a una amplia gama de programas y cursos, sin importar su ubicación geográfica.
+                td <strong>Autogestión del tiempo: </strong>Falta de interacción cara a cara: La ausencia de contacto físico puede disminuir la sensación de comunidad y la interacción espontánea con compañeros e instructores.
+              tr
+                td <strong>Aprendizaje a ritmo propio: </strong>Los estudiantes pueden avanzar a su propio ritmo, revisando el material tantas veces como sea necesario.
+                td <strong>Requiere mayor motivación: </strong>La autonomía que ofrece el aprendizaje en línea también exige que los estudiantes mantengan su motivación sin la estructura tradicional de clases presenciales.
+              tr
+                td <strong>Variedad de recursos digitales: </strong>Los estudiantes tienen acceso a videos, documentos, foros, evaluaciones automatizadas y otros recursos interactivos que enriquecen la experiencia de aprendizaje.
+                td <strong></strong>Distracciones en el entorno personal: Al estudiar desde casa o en otros entornos no tradicionales, los estudiantes pueden enfrentarse a más distracciones que en un aula presencial.
+              tr
+                td <strong>Menor costo: </strong>El aprendizaje en línea suele ser más asequible, ya que elimina los costos de transporte, alojamiento y materiales impresos.
+                td <strong>Dependencia de la tecnología: </strong>Problemas técnicos, como una conexión a internet inestable o falta de acceso a dispositivos adecuados, pueden interrumpir el proceso de aprendizaje.
+    .row.material-complementario
+      h2 MATERIAL COMPLEMENTARIO
+      .col-12.col-md-6.col-lg-7
+        p Los invitamos a explorar el material complementario de este curso, en esta sección encontrará recursos que le permitirán profundizar  y enriquecer su aprendizaje en los temas tratados en esta unidad.
+        p.d-flex.my-4
+          img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+          span Peña, G. E. G. (2021). Las fuentes del derecho. Lecciones de introducción al derecho, 189
+        p.d-flex.my-4
+          img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+          span Suárez, E. (2020). Introducción al derecho. Ediciones UNL
+        p.d-flex.my-4
+          img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+          span De Sousa, M. T. (2019). Introducción al derecho. Temis 
+        p.d-flex.my-4
+          img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+          span Nieto, A. (2019). Una introducción al derecho.
+      .col-12.col-md-6.col-lg-3.offset-lg-1
+        figure
+          img(src='@/assets/componentes/material-complementario.svg', alt='Texto que describa la imagen')
+
+    .bg-full-width.border-top.actividad(style="background-color: #ebf1f5; border-top: 5px solid #f5c145 !important")
+      .p-4.p-md-5
+        #Actividad                
+          <Actividad :cuestionario="cuestionario"/>
+
+</template>
+
+<script>
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema3',
+  components: {
+    BannerInterno,
+  },
+  data: () => ({
+    cuestionario: {
+      tema: 'Sistemas numéricos y álgebra de Boole',
+      titulo: 'Ponte a prueba',
+      introduccion:
+        'Demuestra lo que aprendiste en esta unidad y pon a prueba tus conocimientos.',
+      barajarPreguntas: true,
+      preguntas: [
+        {
+          id: 1,
+          texto:
+            'El estado civil debe acreditarse mediante ______ oficiales, como el acta de nacimiento o matrimonio.',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Informes.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Documentos.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Contratos.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Declaraciones juradas.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 2,
+          texto:
+            'El fallecimiento de una persona natural causa el ______ de su personalidad jurídica.',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Inicio.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Mantenimiento.',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'Cese.',
+              esCorrecta: true,
+            },
+            {
+              id: 'd',
+              texto: 'Incremento.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 3,
+          texto:
+            'Una persona con capacidad de obrar plena puede ______ sus derechos sin restricciones.',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Transferir.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Ceder.',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'Ejercer.',
+              esCorrecta: true,
+            },
+            {
+              id: 'd',
+              texto: 'Renunciar.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 4,
+          texto:
+            'Una empresa de consultoría en Derecho revisa los atributos de personalidad de las personas jurídicas en comparación con las naturales. Al estudiar los derechos, encuentra uno aplica a los dos tipos de persona. ¿Cuál de los siguientes es compartido tanto por personas naturales como jurídicas?',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'La nacionalidad.',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'La posibilidad de poseer bienes.',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'La capacidad de obrar.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'La responsabilidad limitada.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 5,
+          texto:
+            'Fernando está revisando los documentos de su empresa puesto que junto con sus socios se disponen a cambiar de domicilio. Ante la situación se pregunta si habrán repercusiones e los derechos y obligaciones que ostenta su organización. ¿El cambio de domicilio afecta la personalidad jurídica de una persona jurídica en Colombia?',
+          imagen: require('@/assets/actividad/imagen2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto:
+                'No, el cambio de domicilio no afecta la personalidad jurídica, pero debe ser actualizado ante las autoridades.',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto:
+                'Sí, el cambio de domicilio implica la creación de una nueva persona jurídica.',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto:
+                'El domicilio solo puede ser cambiado si lo aprueba una autoridad judicial.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto:
+                'Las personas jurídicas no pueden cambiar de domicilio sin disolverse.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+      ],
+      mensaje_final_aprobado: '¡Felicidades! Has superado la prueba con éxito.',
+      mensaje_final_reprobado:
+        'Te recomendamos repasar nuevamente la unidad para reforzar los conceptos clave antes de volver a intentarlo.',
+    },
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+}
+</script>
+
+<style lang="sass">
+.material-complementario
+  padding-top: 3rem
+  border-top: 9px solid $color-sistema-e
+</style>
